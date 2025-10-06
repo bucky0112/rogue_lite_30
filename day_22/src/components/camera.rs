687644327cc20bb7f5495354c7385e3986f7646d
bucky0112 <bucky0112@gmail.com>
2025-10-06 17:21:18 +0800

    day22

---
+++ day_22/src/components/camera.rs
@@ -0,0 +1,12 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+pub struct CameraFollow {
+    pub speed: f32,
+}
+
+impl CameraFollow {
+    pub fn new(speed: f32) -> Self {
+        Self { speed }
+    }
+}
---
+++ day_22/src/resources/mod.rs
@@ -0,0 +1,13 @@
+pub mod entrance_location;
+pub mod environment_assets;
+pub mod player_death_state;
+pub mod room_assets;
+pub mod sound_effects;
+pub mod world_bounds;
+
+pub use entrance_location::*;
+pub use environment_assets::*;
+pub use player_death_state::*;
+pub use room_assets::*;
+pub use sound_effects::*;
+pub use world_bounds::*;
